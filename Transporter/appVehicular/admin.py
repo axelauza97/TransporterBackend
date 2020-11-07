653c924b72429945721601138890e88819499e78
UDannyf <45443922+UDannyf@users.noreply.github.com>
2020-11-07 10:34:23 -0500

    Update admin.py

--- Transporter/appVehicular/admin.py
+++ Transporter/appVehicular/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import *
+from rest_framework.authtoken.admin import TokenAdmin
 # Register your models here.
 
 
+TokenAdmin.raw_id_fields = ['user']
 admin.site.register(Company)
 admin.site.register(Vehicle)
 admin.site.register(Driver)
@@ -12,4 +14,5 @@ admin.site.register(Fare)
 admin.site.register(Payment)
 admin.site.register(Location)
 admin.site.register(Service)
-
+admin.site.register(Police)
+admin.site.register(Manager)
